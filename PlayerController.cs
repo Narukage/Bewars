5ecea9cab0440ed7fe7b9d45d92a31151b5e7687
Narukage <yolanda.cswag@gmail.com>
2016-12-05 14:07:39 +0100

    Scrip Character Movement

---
+++ PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpHeight;
+	private Animator anim;
+    private bool faceRight;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+        faceRight = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float horizontal = Input.GetAxis("Horizontal");
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2 (GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		} 
+
+		if(Input.GetKey(KeyCode.A))
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2 (-moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
+		}
+
+		anim.SetFloat ("Speed", Mathf.Abs(GetComponent<Rigidbody2D> ().velocity.x));
+        Flip(horizontal);
+	}
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !faceRight || horizontal < 0 && faceRight)
+        {
+            faceRight = !faceRight;
+
+            Vector2 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
+}
